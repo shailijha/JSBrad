420a9c9f6449e3e84f5595eee12d1fe80593ae62
shailijha <shaili_jha@yahoo.co.in>
2019-09-01 00:23:34 -0400

    Data types and type conversion in JS

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 //alert('Hello');
-console.log('Hi!');
-console.table({ a: 1, b: 2 });
-console.error('This is an error');
-console.clear();
-console.time('Hello');
-console.log('Hi!');
-console.log('Hi!');
-console.log('Hi!');
-console.log('Hi!');
-console.log('Hi!');
-console.log('Hi!');
-console.log('Hi!');
-console.timeEnd('Hello');
+// console.log('Hi!');
+// console.table({ a: 1, b: 2 });
+// console.error('This is an error');
+// console.clear();
+// console.time('Hello');
+// console.log('Hi!');
+// console.log('Hi!');
+// console.log('Hi!');
+// console.log('Hi!');
+// console.log('Hi!');
+// console.log('Hi!');
+// console.log('Hi!');
+// console.timeEnd('Hello');
+
+//Primitive
+// const name = 'John Doe';
+
+// console.log(typeof name);
+
+let val;
+//val = String(true);
+//val = 5;
+//val = String([89, 0, 90]);
+//val = String(new Date());
+//val = String(null);
+
+// val = (5).toString();
+val = parseFloat('5hi');
+
+console.log(val);
+console.log(typeof val);
+console.log(val.toFixed());
