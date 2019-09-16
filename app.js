9ddbb55aeb7000bf75ceef5fdd360e6cbdbb0d49
shailijha <shaili_jha@yahoo.co.in>
2019-09-16 12:04:39 -0400

    Date time in js

--- app.js
+++ app.js
@@ -19,15 +19,34 @@
 // console.log(typeof name);
 
 let val;
-//val = String(true);
-//val = 5;
-//val = String([89, 0, 90]);
-//val = String(new Date());
-//val = String(null);
+// //val = String(true);
+// //val = 5;
+// //val = String([89, 0, 90]);
+// //val = String(new Date());
+// //val = String(null);
 
-// val = (5).toString();
-val = parseFloat('5hi');
+// // val = (5).toString();
+// val = parseFloat('5hi');
 
-console.log(val);
-console.log(typeof val);
-console.log(val.toFixed());
+// console.log(val);
+// console.log(typeof val);
+// console.log(val.toFixed());
+
+const today = new Date();
+let birthday = new Date('9-10-1981 11:25:00');
+birthday = new Date('September 11 2019');
+birthday = new Date('9///1/1992');
+
+val = today.getMonth();
+val = today.getDate();
+val = birthday.getDay();
+val = today.getFullYear();
+val = today.getHours();
+val = today.getMinutes();
+val = today.getSeconds();
+val = today.getMilliseconds();
+
+birthday.setMonth(3);
+birthday.setDate(31);
+
+console.log(birthday);
