ac2ca1ababa5c088363c4bffb18058b2da91ab99
shailijha <shaili_jha@yahoo.co.in>
2019-09-22 11:07:42 -0400

    Added window object properties

--- app.js
+++ app.js
@@ -32,21 +32,77 @@ let val;
 // console.log(typeof val);
 // console.log(val.toFixed());
 
-const today = new Date();
-let birthday = new Date('9-10-1981 11:25:00');
-birthday = new Date('September 11 2019');
-birthday = new Date('9///1/1992');
+// const today = new Date();
+// let birthday = new Date('9-10-1981 11:25:00');
+// birthday = new Date('September 11 2019');
+// birthday = new Date('9///1/1992');
 
-val = today.getMonth();
-val = today.getDate();
-val = birthday.getDay();
-val = today.getFullYear();
-val = today.getHours();
-val = today.getMinutes();
-val = today.getSeconds();
-val = today.getMilliseconds();
+// val = today.getMonth();
+// val = today.getDate();
+// val = birthday.getDay();
+// val = today.getFullYear();
+// val = today.getHours();
+// val = today.getMinutes();
+// val = today.getSeconds();
+// val = today.getMilliseconds();
 
-birthday.setMonth(3);
-birthday.setDate(31);
+// birthday.setMonth(3);
+// birthday.setDate(31);
 
-console.log(birthday);
+// console.log(birthday);
+
+// Window Methods/ Objects/ Properties
+
+// window.console.log(123);
+
+// // Alert
+// window.alert('Hi there!');
+
+// Prompt
+// const input = prompt("What's your age?");
+// alert(input);
+
+// Confirm
+// if (confirm('Are you sure?')) {
+//   console.log('Confirmed');
+// } else {
+//   console.log('Cancelled');
+// }
+
+//Outer height and width
+val = window.outerHeight;
+val = window.outerWidth;
+
+val = window.innerHeight;
+val = window.innerWidth;
+
+val = window.scrollY;
+val = window.scrollX;
+
+//Location Object
+val = window.location;
+val = window.location.hostname;
+val = window.location.port;
+val = window.location.href;
+val = window.location.search;
+
+// Redirect
+//window.location.href = 'http://google.com';
+// Reload
+//window.location.reload();
+
+// History object
+// window.history.go(-2);
+val = window.history.length;
+
+// Navigator object
+val = window.navigator;
+val = window.navigator.appName;
+val = window.navigator.appVersion;
+val = window.navigator.userAgent;
+val = window.navigator.platform;
+val = window.navigator.vendor;
+val = window.navigator.language;
+val = window.navigator.geolocation;
+
+console.log(val);
